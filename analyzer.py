46ee12515c0c706964d57968565caa1bfee41b64
Jonathan Wong <chi.jonathanwong@gmail.com>
2018-03-07 00:46:23 -0800

    Clean up Jupter syntax

--- analyzer.py
+++ analyzer.py
@@ -1,47 +1,12 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
+#Read txt file into a dataframe
 data_table = pd.read_csv('data/data.txt',encoding='latin-1')
-data_table
-
-
-# In[2]:
-
-
 data_table = data_table.dropna(subset = ['ZIPCODE'])
-#data_table = data_table[np.isfinite(data_table['ZIPCODE'])]
-data_table
 
-
-# In[3]:
-
-
-#Drop duplicate zipcodes and keep the most recent ones
-duplicated = data_table.duplicated(subset='CAMIS')
-duplicated
-
-
-# In[4]:
-
-
+#Sort then drop duplicates in order to keep lastest inspected date
 data_table = data_table.sort_values(by=['INSPDATE'])
-
 data_table = data_table.drop_duplicates(subset='CAMIS',keep='last')
-data_table
-
-#^^^ WILL SORT THEN KEEP LAST ONES
-
-
-# In[10]:
-
 
 #Drop all rows in the series with zipcodes that appear less than 100 times 
 def display_valid_zips(zip_series):
@@ -51,20 +16,6 @@ def display_valid_zips(zip_series):
     return zip_series
 
 zip_restaurants = display_valid_zips(data_table['ZIPCODE'].value_counts())
-zip_restaurants
-
-#display_valid_zips(data_table['ZIPCODE'].value_counts)
-#data_table['ZIPCODE'].value_counts()
-
-
-# In[9]:
-
-
-#data_table.groupby(['CAMIS', 'ZIPCODE'])['SCORE'].sum().sort_values(ascending=False)
-
-
-# In[55]:
-
 
 #Gather 92 series/list of all 92 restaurants and tally up the average scores
 average_scores = {}
@@ -73,26 +24,19 @@ for k,v in zip_restaurants.iteritems():
     zip_average = zip_df['SCORE'].sum() / v
     
     average_scores.update({k:float('%.1f'%zip_average)})
-    
-print(average_scores)
-                           
-
-
-# In[56]:
-
 
 final_data = []
 for k,v in zip_restaurants.iteritems():
     score = average_scores.get(k)
     data = (k, score, v)
     final_data.append(data)
-    
+
+#Sort by second item in tuple
+final_data = sorted(final_data, key=lambda tup: tup[1], reverse=True)
+
+#Output final results into output.txt
+output = open('output.txt', 'w')
+for data in final_data:
+    output.write(str(data)[1:-1]+ "\n")
+
 print(final_data)
-
-
-# In[57]:
-
-
-sorted_by_second = sorted(final_data, key=lambda tup: tup[1], reverse=True)
-print(sorted_by_second)
-
